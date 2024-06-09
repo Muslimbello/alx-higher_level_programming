b3ad60818d6059506707bcbf2d82c8148dc10e4d
Muslimbello <yuusufyusasif@gmail.com>
2024-06-09 17:32:25 +0100

    redoing task 1

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,7 @@
+import add_0 as add
+a =1
+b = 2
+
+
+if __name__ == "__main__":
+	print(f"{a} + {b} = {add.add(a, b)}")
---
+++ 0x02-python-import_modules/add_0.py
@@ -0,0 +1,11 @@
+def add(a, b):
+	""" My addition function
+
+	Args:
+		a: first number
+		b: second number
+
+	Returns:
+		The return value. a + b
+	"""
+	return (a + b)
